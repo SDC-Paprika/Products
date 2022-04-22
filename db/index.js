1061ef5bc0da5d0a947689238dd948f257921fab
Addison Hernandez <addison.hernandez@gmail.com>
2022-04-21 20:00:57 -0400

    Perform initial connectivity checks
    They worked!

--- db/index.js
+++ db/index.js
@@ -5,7 +5,20 @@ const client = new Client();
 
 client.connect().then(() => console.log('Connected to PostgreSQL!'));
 
-client.query('SELECT $1::text as message', ['Hello, pg!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message);
-  client.end();
-});
+/* test: client has a valid connection */
+// client.query('SELECT $1::text as message', ['Hello, pg!'], (err, res) => {
+//   console.log(err ? err.stack : res.rows[0].message);
+//   client.end();
+// });
+
+/* test: client connects to the actual database */
+// client
+//   .query('SELECT * from products WHERE id = 1')
+//   .then((res) => {
+//     console.log({ res })
+//     console.table(res.rows);
+//   })
+//   .catch(console.error)
+//   .then(() => client.end());
+
+module.exports = client;
