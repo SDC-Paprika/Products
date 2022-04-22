81db06389438526f3e64f0256d25a7b5ce2a3c5c
Addison Hernandez <addison.hernandez@gmail.com>
2022-04-22 19:19:43 -0400

    Modify routes to use camelCase params

--- server/routes.js
+++ server/routes.js
@@ -3,10 +3,10 @@ const controller = require('./controllers');
 
 router.get('/', controller.products.get);
 
-router.get('/:product_id', controller.details.get);
+router.get('/:productId', controller.details.get);
 
-router.get('/:product_id/styles', controller.styles.get);
+router.get('/:productId/styles', controller.styles.get);
 
-router.get('/:product_id/related', controller.related.get);
+router.get('/:productId/related', controller.related.get);
 
 module.exports = router;
