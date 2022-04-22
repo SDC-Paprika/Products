4fa80415df16026a905013b601354fdcc090bf1b
Addison Hernandez <addison.hernandez@gmail.com>
2022-04-22 15:25:47 -0400

    Implement /products endpoint

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,5 +1,18 @@
-const db = require('../../db');
+const { products } = require('../models');
 
 module.exports = {
-  get(req, res) {},
+  get(req, res) {
+    const { query } = req;
+
+    const page = query.page || 1;
+    const count = query.count || 5;
+
+    products
+      .get(page, count)
+      .then((products) => res.status(200).send(products))
+      .catch((err) => {
+        console.error(err.stack);
+        res.sendStatus(500);
+      });
+  },
 };
--- server/models/products.js
+++ server/models/products.js
@@ -1,1 +1,12 @@
-module.exports = {};
+const db = require('../../db');
+
+module.exports = {
+  get(page = 1, count = 5) {
+    const offset = count * (page - 1);
+
+    return db
+      .query('SELECT * from products LIMIT $1 OFFSET $2', [count, offset])
+      .then((result) => result.rows)
+      .catch(console.error);
+  },
+};
