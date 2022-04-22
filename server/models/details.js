a66c03dd89c1a51946b4e4e249629eea4bd18fed
Addison Hernandez <addison.hernandez@gmail.com>
2022-04-22 19:25:09 -0400

    Format query string for readability

--- server/models/details.js
+++ server/models/details.js
@@ -2,13 +2,20 @@ const db = require('../../db');
 
 module.exports = {
   get(productId) {
-    const queryString = `SELECT
-      p.id, p.name, p.slogan, p.description, p.category, p.default_price,
-      (
-        SELECT json_agg(f)
-        FROM (SELECT feature, value FROM features WHERE product_id = $1)
-        AS f
-      ) AS features
+    const queryString = `
+      SELECT
+        p.id,
+        p.name,
+        p.slogan,
+        p.description,
+        p.category,
+        p.default_price,
+        (
+          SELECT json_agg(f)
+          FROM (
+            SELECT feature, value FROM features WHERE product_id = $1
+          ) AS f
+        ) AS features
       FROM products p
       WHERE id = $1`;
 
