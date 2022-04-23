cc973be976f73a5e38bf4ca0d7b32fa557cfe2ef
Addison Hernandez <addison.hernandez@gmail.com>
2022-04-23 11:29:39 -0400

    Refactor queries using query config object style

--- server/models/products.js
+++ server/models/products.js
@@ -3,9 +3,11 @@ const db = require('../../db');
 module.exports = {
   get(page = 1, count = 5) {
     const offset = count * (page - 1);
+    const values = [count, offset];
+    const text = 'SELECT * from products LIMIT $1 OFFSET $2';
 
     return db
-      .query('SELECT * from products LIMIT $1 OFFSET $2', [count, offset])
+      .query({ text, values })
       .then((result) => result.rows)
       .catch(console.error);
   },
---
+++ server/models/related.js
@@ -0,0 +1,19 @@
+const db = require('../../db');
+
+module.exports = {
+  get(productId) {
+    const values = [productId];
+    const text = `
+      SELECT related_product_id
+      FROM related
+      WHERE current_product_id = $1`;
+
+    return db
+      .query({ text, values, rowMode: 'array' })
+      .then(({ rows }) => {
+        console.log({ rows });
+        return rows.flat();
+      })
+      .catch(console.error);
+  },
+};
