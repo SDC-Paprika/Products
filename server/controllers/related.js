5268c3c60bd5453ac6bc2a631a5fb4cb84b9c91b
Addison Hernandez <addison.hernandez@gmail.com>
2022-04-23 11:34:00 -0400

    Add /products/:productId/related endpoint

--- server/controllers/related.js
+++ server/controllers/related.js
@@ -1,5 +1,15 @@
-const db = require('../../db');
+const { related } = require('../models');
 
 module.exports = {
-  get() {},
+  get(req, res) {
+    const { productId } = req.params;
+
+    related
+      .get(productId)
+      .then((results) => res.status(200).send(results))
+      .catch((err) => {
+        console.error(err.stack);
+        res.sendStatus(500);
+      });
+  },
 };
