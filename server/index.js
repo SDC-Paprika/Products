c585a6dfd9b65685533d9dd3ad1366febaf52e93
Addison Hernandez <addison.hernandez@gmail.com>
2022-04-24 08:29:38 -0400

    Add morgan logging middleware

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 // Middleware here
-// TODO: select a logging middleware
+const morgan = require('morgan');
 
 // Router
 const router = require('./routes');
@@ -10,6 +10,7 @@ const router = require('./routes');
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/products', router);
 
